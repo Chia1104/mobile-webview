c6e5d866eb5c5674470d36169386ab746f6eac61
Chia1104 <yuyuchia7423@gmail.com>
2024-01-20 04:41:20 +0800

    test localstorage

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,47 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
+import { useState, useCallback } from "react";
+import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Stack } from "expo-router";
-import { Button, Text } from "tamagui";
+import { Button, Text, Input, View } from "tamagui";
+import * as WebBrowser from "expo-web-browser";
+
+const isUrl = (url: string) => {
+  try {
+    const parsed = new URL(url);
+    return ["http:", "https:"].includes(parsed.protocol);
+  } catch (e) {
+    return false;
+  }
+};
 
 const Index = () => {
+  const [url, setUrl] = useState("");
+  const [error, setError] = useState("");
+
+  const handleOpenWithWebBrowser = useCallback(async () => {
+    if (isUrl(url)) {
+      setError("");
+      await WebBrowser.openBrowserAsync(url);
+      return;
+    }
+    setError("Invalid URL");
+  }, [url]);
   return (
     <SafeAreaView>
-      {/* Changes page title visible on the header */}
-      <Stack.Screen options={{ title: "Home" }} />
-      <View>
-        <Text style={styles.text}>Hello World !</Text>
-        <Button>Click me</Button>
+      <Stack.Screen options={{ title: "IsBe" }} />
+      <View flexDirection="column" justifyContent="center" alignItems="center">
+        <Text style={styles.text}>A simple demo to open a URL in browser</Text>
+        <Input
+          value={url}
+          onChangeText={setUrl}
+          placeholder="Enter URL"
+          marginHorizontal={10}
+          width={300}
+        />
+        <Text style={styles.error}>{error}</Text>
+        <Button onPress={handleOpenWithWebBrowser} marginHorizontal={10}>
+          Open in browser
+        </Button>
       </View>
     </SafeAreaView>
   );
@@ -21,6 +51,13 @@ const styles = StyleSheet.create({
   text: {
     color: "#888",
     fontSize: 18,
+    marginVertical: 20,
+    paddingHorizontal: 10,
+  },
+  error: {
+    color: "red",
+    marginVertical: 10,
+    paddingHorizontal: 10,
   },
 });
 
